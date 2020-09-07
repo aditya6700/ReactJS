849b9ec8833ef2c8b4f7f3fd075df6a008f8e5a7
Sai Aditya <dsaiaditya@gmail.com>
2020-09-07 17:02:25 +0530

    added practice_1 in firstweb

---
+++ firstweb/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+              //practice 1
+
+import './practice_1/index1.css'   // importing css file
+import mydata from './practice_1/mydata.json'  // importing json data file
+ // getting time and date
+var d = new Date().toLocaleDateString();
+var t = new Date().toLocaleTimeString();
+
+ //  creating styles using JS
+
+const h1={
+  textAlign: 'cente',
+    padding: '3rem',
+    color: 'blueviolet',
+}
+
+const imag = "https://picsum.photos/300/300";
+
+ReactDOM.render(
+  <> 
+    <h1 style={h1}> hello world</h1> 
+    <p> fifteen six is {15 *6}</p>
+    <h2> Time is {t} and date is {d} </h2>
+    <br></br>
+    { mydata.map((key,index) =>{
+      return <div>
+        <h3> Temp1 : {key.Temp1} Hum1 : {key.Hum1} __
+        Temp2 : {key.Temp2} Hum2 : {key.Hum2}</h3>
+      </div>
+    }
+    )}
+    <br></br>
+    <img src={imag} alt="randomimage"></img>
+  </>,
+  document.getElementById('root')
+);
+
+
